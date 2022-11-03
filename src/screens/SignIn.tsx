8235efef3bbe8cdf365ebf7178d536e23ff73053
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-11-03 15:02:17 -0300

    importando o button

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,17 @@
-import { Center, Text } from 'native-base';
-import { Loading } from '../components/Loading';
+import { Center, Text, Icon } from 'native-base';
+import Logo from '../assets/logo.svg';
+import { Button } from '../components/Button';
+import { Fontisto } from '@expo/vector-icons';
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor='gray.900' fontFamily='heading'>
-        <Text color='white' fontSize={24}>
-          Sign In
-        </Text>
-     </Center>
+    <Center flex={1} bgColor='gray.900'>
+      <Logo width={212} height={40} />   
+      <Button 
+        title='Entrar com o google' 
+        leftIcon={<Icon as={Fontisto} name='google' color='white' size='md' />}
+        type='SECONDARY'
+      />    
+    </Center>
   );
 }
