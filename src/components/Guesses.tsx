6751c3a1551f2124ed05035e952663f3bcc2c80a
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-11-05 22:47:52 -0300

    Registrando os palpites

--- src/components/Guesses.tsx
+++ src/components/Guesses.tsx
@@ -2,6 +2,7 @@ import { Box, useToast, FlatList } from 'native-base';
 import { useState, useEffect } from 'react';
 import { api } from '../services/api';
 import { Game, GameProps } from '../components/Game';
+import { Loading } from './Loading';
 
 interface Props {
   poolId: string;
@@ -37,9 +38,49 @@ export function Guesses({ poolId }: Props) {
     }
   }
 
+  async function handleGuessConfirm(gameId: string) {
+    try {
+      if(!firstTeamPoints.trim() || !secondTeamPoints.trim()) {
+        return toast.show({
+          title: 'Informe o placar do palpite',
+          placement: 'top',
+          bgColor: 'red.500',
+        });
+      }
+
+      await api.post(`/pools/${poolId}/games/${gameId}/guesses`, {
+        firstTeamPoints: Number(firstTeamPoints),
+        secondTeamPoints: Number(secondTeamPoints),
+      });
+
+      toast.show({
+        title: 'Palpite realizado com sucesso',
+        placement: 'top',
+        bgColor: 'green.500',
+      });
+
+      fetchGames();
+
+    } catch(error) {
+      console.log(error);
+
+      toast.show({
+        title: 'Não foi possível enviar o palpite',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+  }
+
   useEffect(() => {
     fetchGames();
   }, [poolId]);
+
+  if(!isLoading) {
+    return (
+      <Loading />
+    );
+  }
 
   return (
     <FlatList 
@@ -50,7 +91,7 @@ export function Guesses({ poolId }: Props) {
           data={item}
           setFirstTeamPoints={setFirstTeamPoints}
           setSecondTeamPoints={setSecondTeamPoints}
-          onGuessConfirm={() => {}}
+          onGuessConfirm={() => handleGuessConfirm(item.id)}
         />
       )}
     />
