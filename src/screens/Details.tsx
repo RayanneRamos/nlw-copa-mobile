1b8175aff77723d5472f8b28ffe44af5c39d1d5e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-12-07 15:34:48 -0300

    Implementando o sistema de Ranking

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,7 +1,7 @@
 import { HStack, Toast, useToast, VStack } from "native-base";
 import { Header } from "../components/Header";
 import { useRoute } from '@react-navigation/native';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { api } from '../services/api';
 import { Loading } from "../components/Loading";
 import { PoolCardProps } from "../components/PoolCard";
@@ -10,6 +10,7 @@ import { EmptyMyPoolList } from "../components/EmptyMyPoolList";
 import { Option } from "../components/Option";
 import { Share } from 'react-native';
 import { Guesses } from "../components/Guesses";
+import { Ranking } from '../components/Ranking';
 
 interface RouteParams {
   id: string;
@@ -64,7 +65,7 @@ export function Details() {
     <VStack flex={1} bgColor='gray.900'>
       <Header title={id} showBackButton showShareButton onShare={handleCodeShare} />
       {
-        poolDetails._count?.participants > 0 ? 
+        poolDetails._count?.participants > 0 ? (
           <VStack px={5} flex={1}>
             <PoolHeader data={poolDetails} />
             <HStack bgColor='gray.800' p={1} rounded='sm' mb={5}>
@@ -79,10 +80,14 @@ export function Details() {
                 onPress={() => setOptionSelected('ranking')} 
               />
             </HStack>
-            <Guesses poolId={poolDetails.id} code={poolDetails.code} />
+            { optionSelected === 'guesses' && (
+              <Guesses poolId={poolDetails.id} code={poolDetails.code} />
+            ) }
+            { optionSelected === 'ranking' && <Ranking poolId={poolDetails.id} />}
           </VStack>
-        :
+        ) : (
           <EmptyMyPoolList code={poolDetails.code} />
+        )
       }
     </VStack>
   );
